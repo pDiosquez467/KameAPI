8fac3014d34385e0166b5f7185b2c8d3d404a50a
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-06-13 22:52:47 -0300

    Agrego dos endpoints de prueba - Método GET

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,14 +2,42 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.json)
+app.use(express.json())
 
-const usuarios = []
+const usuarios = [
+    {
+        id: 1,
+        nombre: "lioMessi",
+        dinero: 100,
+        personaje_seleccionado: 'Vegeta'
+    }, 
+    {
+        id: 2,
+        nombre: "danielLarusso",
+        dinero: 200,
+        personaje_seleccionado: 'Krilin'
+    },
+]
 const personajes = []
 const desbloqueados = []
 
 app.get('/', (req, res) => {
   res.send('DragonBall App!')
+})
+
+app.get('/api/v1/usuarios', (req, res) => {
+    res.json(usuarios)
+})
+
+app.get('/api/v1/usuarios/:id', (req, res) => {
+    const usuario = usuarios.find(us => us.id == req.params.id)
+    
+    if (usuario === undefined) {
+        res.sendStatus(404)
+        return 
+    }
+    res.json(usuario)
+
 })
 
 app.listen(port, () => {
